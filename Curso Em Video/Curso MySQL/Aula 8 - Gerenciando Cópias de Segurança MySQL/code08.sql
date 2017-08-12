06877e52efeb8470b55d98350af7d362bd623dbb
code36u4r60 <code36u4r60@hotmail.com>
2017-08-12 03:24:59 +0100

    Estudo em MySQL

---
+++ Curso Em Video/Curso MySQL/Aula 8 - Gerenciando Cópias de Segurança MySQL/code08.sql
@@ -0,0 +1,8 @@
+
+-- Crear backup
+-- Dentro do Workbench: Server -> Data Export
+-- Selecionar a BD
+-- Selecionar "Include Create Schema"
+
+-- Importar Base de Dados
+-- Dentro do Workbench: Server -> Data Import
